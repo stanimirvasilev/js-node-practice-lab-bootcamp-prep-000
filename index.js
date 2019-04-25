407ccad8d60726d00897dc471384aebae1b4c09f
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2019-04-25 10:56:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Stanimir"
-var height = "188"
+var height = 188;
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(height+1)
